64eddb9c1e1bf5c6091e6a1708a9ed31ded75e5e
AndreyTarkaniy <endrubass@gmail.com>
2023-05-01 00:27:27 +0300

    add task-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+
 import { Profile } from 'components/profile/Profile';
 import { Statistics } from 'components/statistics/Statistics';
 import { FriendList } from 'components/friendList/FriendList';
+import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -30,6 +33,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
