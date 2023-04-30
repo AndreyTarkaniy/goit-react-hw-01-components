efe04462676a989e5eb96bdfdba8959c48d91ad0
AndreyTarkaniy <endrubass@gmail.com>
2023-04-30 21:49:34 +0300

    add task-1 and task-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import user from 'data/user.json';
+import data from 'data/data.json';
+import { Profile } from 'components/profile/Profile';
+import { Statistics } from 'components/statistics/Statistics';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        textAlign: 'center',
+        listStyle: 'none',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
     </div>
   );
 };
